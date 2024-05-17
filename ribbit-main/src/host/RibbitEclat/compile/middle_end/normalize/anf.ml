848335e355a6c78f9c78cf43ca09883456f63daf
yokyann <67327884+yokyann@users.noreply.github.com>
2024-05-17 22:36:14 +0200

    Add files via upload

---
+++ ribbit-main/src/host/RibbitEclat/compile/middle_end/normalize/anf.ml
@@ -0,0 +1,199 @@
+open Ast
+open Ast_subst
+
+open Pattern
+
+(**
+
+  performs ANF + let-floating + copy propagation
+  
+  expression in ANF-form are defined as follows:
+
+  e ::=
+        xc
+      | fun x -> e
+      | fix f (fun x -> e)
+      | xc xc
+      | if xc then e else e
+      | match xc with c -> e | ... | _ -> e end
+      | match xc with inj p -> e | ... | {_ -> e}? end
+      | reg_l (fun p -> e) last xc
+      | exec_l e xc
+      | let f = fun x -> e in e
+      | let f = fix f' (fun x -> e) in e
+      | let x = e in e
+      
+
+      | ref e         ;; references
+      | x := xs
+      | !x
+      
+      | x[xc]
+      | x.length
+      | x[xc] <- xc
+
+  xc := (xc,xc ... xc)
+      | c
+      | x
+      | x.length
+      | x.size
+      | fst x | snd x | ...
+*)
+
+let rec is_xc (e:e) : bool =
+  match un_deco e with
+  | E_deco(e,_) -> is_xc e
+  | E_var _ | E_const _ -> true
+  | E_tuple(es) -> List.for_all is_xc es
+  | E_app(E_const(Op(GetTuple _)),e) -> is_xc e
+  | E_array_length _ | E_matrix_size _ -> true
+  | _ -> false
+
+(** [plug e ctx] plugs expression [e] into context [ctx], i.e., returns
+   [ctx e] if [is_xc e] is true, otherwise [let y = e in ctx y]
+   with [y] a fresh name. *)
+let plug (e:e) (context: e -> e) : e =
+  if is_xc e then context e else
+  let x = gensym () in
+  E_letIn(P_var x,e,context (E_var x))
+
+(** [plug_n es ctx] plugs expresions [es] into context [ctx]
+    from left to right *)
+let plug_n (es:e list) (context : e list -> e) : e =
+  let rec plug_n_aux aas es context =
+    match es with
+    | [] -> context (List.rev aas)
+    | e::es' -> plug e @@ fun v -> plug_n_aux (v::aas) es' context
+  in
+  plug_n_aux [] es context
+
+(** [anf e] puts expression [e] in ANF-form *)
+let rec anf (e:e) : e =
+  let rec glob (e:e) : _ * e =
+  match e with
+  | E_deco _ ->
+      Ast_undecorated.still_decorated e
+  | E_var _ | E_const _ -> [],e
+  | E_fun(x,e1) ->
+      [],E_fun(x,anf e1)
+  | E_fix(f,(x,e1)) ->
+      [],E_fix(f,(x,anf e1))
+  | E_if(e1,e2,e3) ->
+      let ds1,e1' = glob e1 in
+      ds1, plug e1' @@ fun xc1 ->
+      E_if(xc1,anf e2,anf e3)
+  | E_case(e1,hs,e_els) ->
+      let ds1,e1' = glob e1 in
+      ds1,plug e1' @@ fun xc ->
+      E_case(xc,List.map (fun (c,e) -> c,anf e) hs,anf e_els)
+  | E_match(e1,hs,eo) ->
+      let ds1,e1' = glob e1 in
+      ds1,plug (anf e1') @@ fun xc ->
+      E_match(xc,List.map (fun (x,(p,e)) -> x,(p,anf e)) hs,Option.map anf eo)
+ (* | E_letIn(P_var f,(E_fix(g,(p,e1))),e2) when f <> g ->
+      assert (not (pat_mem f p) && not (pat_mem g p));
+      anf @@ E_letIn(P_var f,E_fix(f,(p,subst_e g (E_var f) e1)),e2)*)
+  | E_letIn(P_tuple ps,E_tuple es,e2) ->
+      glob (List.fold_right2 (fun p e acc -> E_letIn(p,e,acc)) ps es e2)
+  | E_letIn(p,e1,e2) ->
+      let ds1,e1' = glob e1 in
+      let e' = anf e2 in
+      ds1,((*match p,e1' with 
+           | P_unit,E_const Unit -> e'
+           | _ -> if is_xc e1' then subst_p_e p e1' e' else*)   (* bug subst_p_e ici ! *)
+           E_letIn(p,e1',e'))
+  | E_tuple(es) ->
+      let dss,es' = List.split (List.map glob es) in
+      List.concat dss, 
+      plug_n es' @@ fun xs -> E_tuple(xs)
+  | E_app(E_const _ as ec,e2) ->
+      let ds2,e2' = glob e2 in
+      ds2,plug e2' @@ fun x2 ->
+      E_app(ec,x2)
+  | E_app(e1,e2) ->
+      let ds1,e1' = glob e1 in
+      let ds2,e2' = glob e2 in
+      ds1@ds2,plug e1' @@ fun xc1 ->
+      plug e2' @@ fun xc2 ->
+      E_app(xc1,xc2)
+  | E_reg((p,e1),e0,l) ->
+      let ds0,e0' = glob e0 in
+      ds0,plug e0' @@ fun xc0 ->
+      E_reg((p,anf e1),xc0,l)
+  | E_exec(e1,e0,l) ->
+      let ds0,e0' = glob e0 in
+      ds0,plug e0' @@ fun xc0 ->
+      E_exec(anf e1,xc0,l) 
+  | E_ref(e1) ->
+      let ds1,e1' = glob e1 in
+      ds1,plug e1' @@ fun xc1 ->
+      E_ref(xc1)
+  | E_get(e1) ->
+      let ds1,e1' = glob e1 in
+      ds1,plug e1' @@ fun xc1 ->
+      E_get(xc1)
+  | E_set(e1,e2) ->
+      let ds1,e1' = glob e1 in
+      let ds2,e2' = glob e2 in
+      ds1@ds2,plug e1' @@ fun xc1 ->
+      plug e2' @@ fun xc2 ->
+      E_set(xc1,xc2)
+  | E_array_length _ ->
+      [],e
+  | E_local_static_array(e1,loc) ->
+      let ds1,e1' = glob e1 in
+      ds1,plug e1' @@ fun xc1 ->
+              E_local_static_array(xc1,loc)
+  | E_array_get(x,e1) ->
+      let ds1,e1' = glob e1 in
+      ds1,plug e1' @@ fun xc1 ->
+          E_array_get(x,xc1)
+  | E_array_set(x,e1,e2) ->
+      let ds1,e1' = glob e1 in
+      let ds2,e2' = glob e2 in
+      ds1@ds2,plug e1' @@ fun xc1 ->
+              plug e2' @@ fun xc2 ->
+              E_array_set(x,xc1,xc2)
+  | E_local_static_matrix(e1,es,loc) ->
+      let ds1,e1' = glob e1 in
+      let dss,es' = List.split (List.map glob es) in
+      List.concat dss@ds1,
+      plug e1' @@ fun xc1 ->
+      plug_n es' @@ fun xs ->
+      E_local_static_matrix(xc1,xs,loc)
+  | E_matrix_size _ ->
+      [],e
+  | E_matrix_get(x,es) ->
+      let dss,es' = List.split (List.map glob es) in
+      List.concat dss,
+      plug_n es' @@ fun xs -> E_matrix_get(x,xs)
+  | E_matrix_set(x,es,e2) ->
+      let dss,es' = List.split (List.map glob es) in
+      let ds2,e2' = glob e2 in
+      List.concat dss@ds2,
+      plug_n es' @@ fun xs ->
+      plug e2' @@ fun xc2 ->
+      E_matrix_set(x,xs,xc2)
+  | E_par(es) ->
+      [],E_par(List.map anf es)
+  (*| E_absLabel(l,e1) ->
+      E_absLabel(l,anf e1)
+  | E_appLabel(e1,l,lc) ->
+      E_appLabel(anf e1,l,lc)*)
+  | E_for(x,e_st1,e_st2,e3,loc) ->
+      [],E_for(x,anf e_st1,anf e_st2,anf e3,loc)
+      (* NB: [e_st1] and [e_st2] are *not* moved up with `plug` *)
+  | E_generate((p,e1),e2,e_st3,loc) ->
+      [],plug (anf e2) @@ fun xc ->
+      E_generate((p,anf e1),xc,anf e_st3,loc) 
+      (* NB: [e_st3] is *not* moved up with `plug` 
+*)
+  in 
+  let ds,e' = glob e in 
+  Ast_mapper.declare ds e'
+
+(** [anf e] puts program [pi] in ANF-form *)
+let anf_pi (pi:pi) : pi =
+  { pi with main = anf pi.main } (*
+  { pi with main = anf pi.main }*)
+
---
+++ ribbit-main/src/host/RibbitEclat/compile/middle_end/transform/specialize.ml
@@ -0,0 +1,199 @@
+open Ast
+open Ast_subst
+
+let has_changed = ref false
+
+let applyed e =
+  let open Ast in
+
+  let rec applied_list xs es =
+    List.fold_left (fun acc ei -> acc ++ aux xs ei) SMap.empty es
+  and aux xs = function
+  | E_deco(e,_) ->
+      aux xs e
+  | E_var x ->
+      SMap.empty
+  | E_const _ ->
+      SMap.empty
+  | E_if(e1,e2,e3) ->
+      aux xs e1 ++ aux xs e2 ++ aux xs e3
+  | E_case(e1,hs,e_els) ->
+      aux xs e1 ++
+      List.fold_left (fun acc (c,ei) -> (acc ++ aux xs ei)) SMap.empty hs ++
+      aux xs e_els
+  | E_match(e1,hs,eo) ->
+      let s = aux xs e1 ++
+              List.fold_left (fun acc (inj,(p,ei)) ->
+                let ys = vars_of_p p in
+                let xs' = xs ++ ys in
+                (acc ++ aux xs' ei)) SMap.empty hs in
+      (match eo with
+       | None -> s
+       | Some e -> s ++ aux xs e)
+  | E_letIn(p,e1,e2) ->
+      let ys = vars_of_p p in
+      let xs' = xs ++ ys in
+      aux xs e1 ++ aux xs' e2
+  | E_app(E_var f,e2) ->
+      (if SMap.mem f xs then SMap.empty else SMap.singleton f ())
+      ++ aux xs e2
+  | E_app(e1,e2) ->
+      aux xs e1 ++ aux xs e2
+  | E_fun(p,e1) ->
+      let ys = vars_of_p p in
+      let xs' = xs++ys in
+      aux xs' e1
+  | E_fix(f,(p,e)) ->
+      let ys = vars_of_p p in
+      let xs' = SMap.add f () @@ (xs++ys) in
+      aux xs' e
+  | E_tuple(es) ->
+      applied_list xs es
+  | E_reg((p,e1), e0,_) ->
+      let ys = vars_of_p p in
+      let xs' = xs ++ ys in
+      aux xs' e1 ++ aux xs e0
+  | E_exec(e1,e2,_) ->
+      aux xs e1 ++ aux xs e2
+  | E_ref(e1) ->
+      aux xs e1
+  | E_get(e1) ->
+      aux xs e1
+  | E_set(e1,e2) ->
+      aux xs e1 ++ aux xs e2
+  | E_local_static_array(e1,_) ->
+      aux xs e1
+  | E_array_length _ ->
+      SMap.empty
+  | E_array_get(_,e1) ->
+      aux xs e1
+  | E_array_set(_,e1,e2) ->
+      aux xs e1 ++ aux xs e2
+  | E_local_static_matrix(e1,es,_) ->
+      aux xs e1 ++ applied_list xs es
+  | E_matrix_size _ ->
+      SMap.empty
+  | E_matrix_get(_,es) ->
+      applied_list xs es
+  | E_matrix_set(_,es,e2) ->
+      applied_list xs es ++ aux xs e2
+  | E_par(es) ->
+      applied_list xs es 
+  | E_for(i,e_st1,e_st2,e3,_) ->
+      aux xs e_st1 ++ aux xs e_st2 ++
+      (let xs' = SMap.add i () xs in
+       aux xs' e3)
+  | E_generate((p,e1),e2,e_st3,_) ->
+      let ys = vars_of_p p in
+      (let xs' = xs ++ ys in
+       aux xs' e1) ++ aux xs e2 ++ aux xs e_st3
+  in
+  aux SMap.empty e
+
+
+let hof x e =
+  match e with
+  | E_fun(p,e) ->
+      let xs = applyed e in
+      vars_of_p p |> SMap.filter (fun x _ -> SMap.mem x xs) |> SMap.is_empty |> not
+
+  | E_fix(g,(p,e)) ->
+      let xs = applyed e in
+      vars_of_p p |> SMap.filter (fun y _ -> y <> g && y <> x && SMap.mem y xs) |> SMap.is_empty |> not
+  | _ -> false
+
+let p_without_fun f p =
+  let t = try Hashtbl.find Typing.signatures f with Not_found -> Types.unknown() (*TODO : check *) in
+                     let open Types in
+                     let targ = match canon t with T_fun{arg} -> arg | _ -> Types.unknown() (* assert false*) in
+                     let rec remove_fun t p = match canon t,p with 
+                      | _,P_unit -> P_unit
+                      | T_tuple ts,P_tuple ps -> P_tuple (List.map2 remove_fun ts ps)
+                      | T_fun{arg},_ -> P_unit
+                      | T_var{contents=Ty t},p -> remove_fun t p
+                      | _ -> p in
+  remove_fun targ p
+
+
+
+let specialize ds e =
+  let open Ast in
+  let rec spec funcs e =
+    match e with
+    | E_app(E_const _,_) -> e
+    | E_app(E_var f,xc2) ->
+        (match SMap.find_opt f funcs with
+        | None -> e
+        | Some (E_tuple[E_var g;E_fix(_,(p,_))]) ->
+           let p' = p_without_fun f p in
+           let rec aux xc2 p =
+             match xc2,p with
+             | _,P_unit -> E_const Unit
+             | E_var _, _ -> xc2
+             | E_tuple es, P_tuple ps -> E_tuple (List.map2 aux es ps)
+             | e,_ -> e
+            in E_app(E_var f,aux xc2 p')
+        (* | Some ((E_var _ | E_tuple _) as pat_e) ->  Ast_subst.subst_p_e (exp2pat pat_e) E_app(E_var x,xc2)*)
+        | Some (E_fun(p,e1)) -> Anf.anf @@ E_letIn(p,xc2,e1)
+        | Some (E_fix(x,(p,e1))) ->
+              let p' = p_without_fun f p in
+              E_letIn(p,xc2,
+                let z = gensym ~prefix:x () in
+                let e1' = (* Ast_subst.subst_p_e p (Pattern.pat2exp p')*) e1 in
+                E_letIn(P_var z, E_fix(x,(p',e1')), E_app(E_var z,Pattern.pat2exp p')))
+
+        | Some _ ->
+            assert false (* ill typed *)
+      )
+    | E_app(e1,xc) ->
+        E_app(spec funcs e1,xc)
+    | E_letIn(P_var x,((E_var _ | E_tuple _) as xc1),e2) ->
+        (* copy propagation to remove aliasing of global functions *)
+        spec funcs (Ast_subst.subst_e x xc1 e2)
+   (*  | E_letIn(P_var x,(E_fix(f,(p,e1))),e2) ->
+        let p' = 
+                let t = try Hashtbl.find Typing.signatures f with Not_found -> Types.unknown() (*TODO : check *) in
+                     let open Types in
+                     let targ = match canon t with T_fun{arg} -> arg | _ -> Types.unknown() (* assert false*) in
+                     let rec keep_fun t p = match canon t,p with 
+                      | _,P_unit -> P_unit
+                      | T_tuple ts,P_tuple ps -> P_tuple (List.map2 keep_fun ts ps)
+                      | T_fun{arg},P_var x -> P_var x
+                      | T_var{contents=Ty t},p -> keep_fun t p
+                      | _ -> P_unit in
+                      keep_fun targ p
+              in
+
+        let e1' = spec (SMap.add x (Pattern.pat2exp p') funcs) e1 in
+        if hof x e1 then spec (SMap.add x e1' funcs) e2 else
+        E_letIn(P_var x,e1',spec funcs e2)*)
+    | E_letIn(P_var x,(E_fix(x',(p,_)) as e1),e2) ->
+        let e1' = spec (SMap.add x' (E_tuple[E_var x;e1]) funcs) e1 in
+        if hof x e1 then (has_changed := true; spec (SMap.add x e1' funcs) e2) 
+        else
+        E_letIn(P_var x,e1',spec funcs e2)
+    | E_letIn(P_var x,((E_fun _) as e1),e2) ->
+        let e1' = spec funcs e1 in
+        if hof x e1 then (has_changed := true; spec (SMap.add x e1' funcs) e2)
+        else
+        E_letIn(P_var x,e1',spec funcs e2)
+    | E_letIn(p,e1,e2) ->
+        E_letIn(p,spec funcs e1,spec funcs e2)
+    | e -> Ast_mapper.map (spec funcs) e
+  in
+  spec SMap.empty e
+
+
+
+let rec list_update (x,v) = function
+| [] -> []
+| (y,v')::l -> if x = y then (x,v)::l else (y,v')::list_update (x,v) l
+
+
+let specialize_pi pi =
+  has_changed := false;
+  let _ = Typing.typing_with_argument ~collect_sig:true pi [] in
+  (* let pi = Anf.anf_pi pi in*) (* already in anf-form *)
+  let main = specialize [] pi.main in
+  { pi with main }
+  |> Propagation.propagation_pi    (* needed *)
